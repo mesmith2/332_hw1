5a28d9b4b4d388c36d9467f0c3099068d1c08d0b
mesmith2 <mesmith2@uab.edu>
2025-01-23 23:00:12 +0000

    commiting full hw

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 # how to run: ./mesmith2_HW01
 
 # functions initialized  before main()
-# main function prompts user to enter input for each function, then calls function on input and will print output.
+# main function prompts user to enter input for each function, then calls function on users input and will print output.
 
 # primeOrFactorial() returns char*, pointer to the characters being returned, and takes an int n as input;
 # use if else loops for cases: n <= 1; n is prime; n!
@@ -19,5 +19,8 @@
 # replaceEvenWithZero() takes an int* array as input and returns a new int array[]. in main, user is promted to enter
 # how many array elements they would like to enter, then prompted to enter such elements. function is then called to replace even numbers with zeros; within function: static int b[] initialized for return value, using for loop to itterate through original array, calculate if current array element is even, if even, set b[current index value] = 0, else original array contents will be copied to b[]. b is static, otherwise there would not be a valid pointer to memory location to be returned 
 
+# evenSqaure() takes an integer n as input, and returns a boolian true or false. The input int is sqaured, n*n, if the product is even funtion returns true, else the product is odd function returns false. 
 
+# countVowels() takes a string of chars as input, and returns the output as an int representation number of vowels in # string. in main: prompts user to enter string, uses fgets() to read in string into var. s. implemented getchar() after previous scanf()'s to clear the input buffer. deleted last character of string if the character was a new line. within countVowels function, calculate the length of input string, itterate through input string with for loop with int i starting at 0, constraint i < length of input str., compare each element of str to vowel characters, increment local var. count when element = vowel.  
 
+# rep. link : https://github.com/mesmith2/332_hw1
--- mesmith2_HW01.c
+++ mesmith2_HW01.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 //declare functions before calling them
 char* primeOrFactorial(int n);
 int UABIndesSum(int arr[]);
 int* replaceEvenWithZero(int arr[], int n);
+bool evenSquare(int n);
+int countVowels(char* s);
 
 int main(){
     //prime or factorial 
     int pOrf;
     printf("Enter a positve integer: ");
     scanf("%d", &pOrf);
+    getchar(); // getchar after every scanf to clear input buffer for upcoming fgets()
     printf("%s\n", primeOrFactorial(pOrf));
 
     // UAB Index sum
@@ -19,6 +23,7 @@ int main(){
     for (int i = 0; i < 4; i ++){
         scanf("%d", &a[i]);
     }
+    getchar();
     int ans = UABIndesSum(a);
         printf("number of elements that equal their index: %d \n", ans);
 
@@ -27,17 +32,47 @@ int main(){
     int numberOfEl = 0;
     printf("\nEnter number of array elements (up to 10): ");
     scanf("%d", &numberOfEl);
+    getchar();
 
     printf("Enter %d array elements: ", numberOfEl);
     for (int i = 0; i < numberOfEl; i ++){
         scanf("%d", &b[i]);
     }
+    getchar();
     int *bb = replaceEvenWithZero(b, numberOfEl);
     printf("Replace even #'s with 0's: ");
     for (int i = 0; i < numberOfEl; i ++){
         printf("%d ", bb[i]);
     }
-    printf("\n \n");
+
+    // evenSquare
+    int evenOrOdd;
+    printf("\n\nEnter an integer: ");
+    scanf("%d", &evenOrOdd );
+    getchar();
+    bool tOrf = evenSquare(evenOrOdd);
+    if(tOrf){
+        printf("True, %d is an even sqaure.\n", evenOrOdd);
+    }
+    else{
+        printf("False, %d is an odd sqaure.\n", evenOrOdd);
+    }
+
+    //countVowels
+    char s[25];
+    printf("Enter a string of characters up to 25(a-z/A-Z): ");
+
+    fgets(s, sizeof(s), stdin);
+// removing new line character left from scanf
+    size_t len = strlen(s);
+    if( len> 0 && s[len-1]== '\n'){
+    s[len-1] = '\0';
+    }
+   
+    //scanf("%s", s);
+    int numV = countVowels(s);
+    printf("There are %d vowels in the input string.\n\n", numV);
+    
 
 
     return 0;
@@ -96,4 +131,26 @@ int* replaceEvenWithZero(int arr[], int n){
     }
 }
     return b;
+}
+
+bool evenSquare(int n){
+    if((n*n) % 2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+// not char* s[]--> represents an array of strings 
+int countVowels(char* s){
+size_t len = strlen(s);
+ int count = 0;
+ for(int i = 0; i < len; i++){
+    if(s[i] == 'a' || s[i] == 'A') {count ++;}
+    else if(s[i] == 'e' || s[i] == 'E'){count ++;}
+    else if(s[i] == 'i' || s[i] == 'I'){count ++;}
+    else if(s[i] == 'o' || s[i] == 'O'){count ++;}
+    else if(s[i] == 'u' || s[i] == 'U'){count ++;}
+ }
+ return count;
 }
